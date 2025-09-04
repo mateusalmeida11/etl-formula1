c89e357ea681ab5e493f1b541fd4d12b9df893ce
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-04 08:45:55 -0300

    refactor: adicionando mock para testar o status_code da funcao

--- tests/teste_request/test_request_utils.py
+++ tests/teste_request/test_request_utils.py
@@ -1,7 +1,11 @@
-from formula_1_etl.utils.get_api import get_request
+from unittest.mock import patch
+
+from formula_1_etl.utils.get_api import request_url
 
 
-def test_status_200_request_api():
+@patch("formula_1_etl.utils.get_api.requests.Session.get")
+def test_status_200_request_api(mock_get):
+    mock_get.return_value.status_code = 200
     url = "https://api.jolpi.ca/ergast/f1/2025/races"
-    response = get_request(url=url)
+    response = request_url(url=url)
     assert response.status_code == 200
