f5c2b161e6459d95120a14b7806a65c4b8321f3b
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-04 14:28:13 -0300

    refactor: mudanca no parametro que a funcao recebia

--- formula_1_etl/utils/get_api.py
+++ formula_1_etl/utils/get_api.py
@@ -10,13 +10,15 @@ class RequestError(Exception):
         self.response_body = response_body
 
 
-def request_url(url):
+def request_url(endpoint):
     retry_strategy = Retry(
         total=3,
         status_forcelist=[408, 429, 500, 502, 503, 504],
         allowed_methods=["GET"],
         backoff_factor=1,
     )
+    url = "https://api.jolpi.ca/ergast/f1/"
+    url += endpoint
     adapter = HTTPAdapter(max_retries=retry_strategy)
     http = requests.Session()
     http.mount("https://", adapter)
