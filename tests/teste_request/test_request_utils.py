722ec8cc42e5b0dbc07e4ea1872170cdc0ccafd1
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-04 16:06:14 -0300

    test: new - validar timeout

--- tests/teste_request/test_request_utils.py
+++ tests/teste_request/test_request_utils.py
@@ -1,7 +1,7 @@
 from unittest.mock import MagicMock, patch
 
 import pytest
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, Timeout
 
 from formula_1_etl.utils.get_api import RequestError, request_url
 
@@ -38,4 +38,19 @@ def test_status_404_error_rout_endpoint(mock_get):
     e = excinfo.value
     assert e.status_code == 404
     assert "The requested resource was not found on this server" in e.response_body
+    assert e.endpoint == endpoint
+
+
+@patch("formula_1_etl.utils.get_api.requests.Session.get")
+def test_erro_timeout(mock_get):
+    mock_response = MagicMock()
+    mock_response.raise_for_status.side_effect = Timeout
+    mock_get.return_value = mock_response
+    endpoint = "2025321/races"
+    with pytest.raises(Timeout) as excinfo:
+        request_url(endpoint=endpoint)
+
+    e = excinfo.value
+    assert e.status_code is None
+    assert "Erro Devido a Timeout" in e.response_body
     assert e.endpoint == endpoint
