dffbfc449774caa659f5f06e1d11826bb4e9609d
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-05 14:51:58 -0300

    create new_method for to handle paginated requests

---
+++ formula_1_etl/utils/handle_request.py
@@ -0,0 +1,20 @@
+from formula_1_etl.utils.get_api import request_url
+
+
+def handle_requests(endpoint):
+    total = float("inf")
+    offset = 0
+    limit = 100
+
+    params = {"offset": offset, "limit": limit}
+
+    data = []
+
+    while offset < total:
+        response = request_url(endpoint=endpoint, params=params).json()
+        data.append(response)
+        total = int(response["MRData"]["total"])
+        offset += limit
+        params["offset"] = offset
+
+    return data
