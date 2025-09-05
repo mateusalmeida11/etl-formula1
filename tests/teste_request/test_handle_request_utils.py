f9dcb19d377afbcde8491f94518b8016260f935e
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-05 15:52:22 -0300

    test: new - validando logica de paginacao

--- tests/teste_request/test_handle_request_utils.py
+++ tests/teste_request/test_handle_request_utils.py
@@ -32,3 +32,50 @@ def test_retorno_da_lista_maior_que_zero(mock_get):
     endpoint = "/seasons"
     data = handle_requests(endpoint=endpoint)
     assert len(data) > 0
+
+
+@patch("formula_1_etl.utils.get_api.requests.Session.get")
+def test_logica_paginacao(mock_get):
+    # página 1
+    response_page1 = MagicMock()
+    response_page1.status_code = 200
+    response_page1.json.return_value = {
+        "MRData": {
+            "limit": "2",
+            "offset": "0",
+            "total": "5",
+            "SeasonTable": {"Seasons": [{"season": "1950"}, {"season": "1951"}]},
+        }
+    }
+
+    # página 2
+    response_page2 = MagicMock()
+    response_page2.status_code = 200
+    response_page2.json.return_value = {
+        "MRData": {
+            "limit": "2",
+            "offset": "2",
+            "total": "5",
+            "SeasonTable": {"Seasons": [{"season": "1952"}, {"season": "1953"}]},
+        }
+    }
+
+    # página 3
+    response_page3 = MagicMock()
+    response_page3.status_code = 200
+    response_page3.json.return_value = {
+        "MRData": {
+            "limit": "2",
+            "offset": "4",
+            "total": "5",
+            "SeasonTable": {"Seasons": [{"season": "1954"}]},
+        }
+    }
+
+    # side_effect = retorna cada mock em sequência
+    mock_get.side_effect = [response_page1, response_page2, response_page3]
+    endpoint = "/seasons"
+    data = handle_requests(endpoint=endpoint, limit=2)
+    page1 = data[0]["MRData"]["SeasonTable"]["Seasons"]
+    assert len(data) == 3
+    assert len(page1) == 2
