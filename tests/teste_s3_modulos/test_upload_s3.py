54bec2b71c5e1e742a8d73c58021442f3c0201da
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-06 16:41:01 -0300

    teste: new - validar sucesso de uploado para o s3

---
+++ tests/teste_s3_modulos/test_upload_s3.py
@@ -0,0 +1,41 @@
+import json
+
+import boto3
+from moto import mock_aws
+
+from formula_1_etl.utils.aws.S3 import S3
+
+
+@mock_aws
+def test_upload_s3_success():
+    bucket_name = "etl-formula-1"
+
+    client = boto3.client("s3", region_name="us-east-1")
+    client.create_bucket(Bucket=bucket_name)
+
+    key = "raw/seasons/2025/09/01/all_seasons.json"
+    body = {
+        "MRData": {
+            "xmlns": "",
+            "series": "f1",
+            "url": "https://api.jolpi.ca/ergast/f1/seasons/",
+            "limit": "30",
+            "offset": "0",
+            "total": "76",
+            "SeasonTable": {
+                "Seasons": [
+                    {
+                        "season": "1950",
+                        "url": "https://en.wikipedia.org/wiki/1950_Formula_One_season",
+                    }
+                ]
+            },
+        }
+    }
+    json_data = json.dumps(body, indent=4)
+    s3 = S3(bucket_name=bucket_name)
+    response = s3.upload_file(data=json_data, key=key)
+
+    status_code = response["ResponseMetadata"]["HTTPStatusCode"]
+    assert status_code == 200
+    assert "ETag" in response
