a57c44091c107c9814e9913c740be787a1102ab8
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-06 16:48:35 -0300

    folder: criacao do modulo aws

---
+++ formula_1_etl/utils/aws/S3.py
@@ -0,0 +1,20 @@
+import os
+
+import boto3
+
+
+class S3:
+    def __init__(self, bucket_name):
+        self.s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=os.environ.get("ACCESS_KEY"),
+            aws_secret_access_key=os.environ.get("SECRET_ACCESS_KEY"),
+            region_name=os.environ.get("REGION"),
+        )
+        self.bucket_name = bucket_name
+
+    def upload_file(self, data, key):
+        response = self.s3_client.put_object(
+            Body=data, Key=key, Bucket=self.bucket_name, ContentyType="application/json"
+        )
+        return response
