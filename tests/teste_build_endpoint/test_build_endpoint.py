89a01e62b0dc985c662b2752ed963e3b54fcb347
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-10 10:14:30 -0300

    test: new - criacao de um novo teste para parte do season

--- tests/teste_build_endpoint/test_build_endpoint.py
+++ tests/teste_build_endpoint/test_build_endpoint.py
@@ -5,3 +5,10 @@ def test_build_endpoint_category():
     category = "seasons"
     endpoint = build_endpoint(category=category)
     assert "seasons" == endpoint
+
+
+def test_build_endpoint_category_season():
+    category = "seasons"
+    season = "2025"
+    endpoint = build_endpoint(category=category, season=season)
+    assert "2025/seasons" == endpoint
