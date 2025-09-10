a9a48aad731898cd2847a722d9ebb85fd09fdac5
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-10 10:09:17 -0300

    test: refactor para adicionar o assert

--- tests/teste_build_endpoint/test_build_endpoint.py
+++ tests/teste_build_endpoint/test_build_endpoint.py
@@ -4,4 +4,4 @@ from formula_1_etl.utils.build_endpoint import build_endpoint
 def test_build_endpoint_category():
     category = "seasons"
     endpoint = build_endpoint(category=category)
-    return endpoint
+    assert "seasons" == endpoint
