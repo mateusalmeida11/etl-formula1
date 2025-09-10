9e6c78b9123312b9c381d13f2cfb9d37c09b63bf
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-10 09:06:43 -0300

    refactor: adpatando nomenclatura raiz para melhores padroes

--- formula_1_etl/utils/create_name_files.py
+++ formula_1_etl/utils/create_name_files.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 
-def create_root_path(layer_name, endpoint):
+def create_root_path(layer_name, category):
     today = datetime.today()
-    return f"{layer_name}/{endpoint}/{today.year}/{today.month}/{today.day}"
+    return (
+        f"{layer_name}/{category}/year={today.year}/month={today.month}/day={today.day}"
+    )
