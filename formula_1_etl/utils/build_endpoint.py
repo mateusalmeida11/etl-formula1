a0c2fda6382a63a3fbdd73a48ddf7aefeaa9ab09
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-10 10:48:36 -0300

    refactor: adicao da parte de rounds

--- formula_1_etl/utils/build_endpoint.py
+++ formula_1_etl/utils/build_endpoint.py
@@ -1,6 +1,10 @@
-def build_endpoint(category, season=None):
+def build_endpoint(category, season=None, rounds=None):
     pieces = []
     if season:
         pieces.append(season)
+
+    if rounds:
+        pieces.append(rounds)
+
     pieces.append(category)
     return "/".join(pieces)
