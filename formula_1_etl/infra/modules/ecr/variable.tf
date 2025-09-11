fe8fd1f381702df991dd7ed8d2f970986c186818
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-11 13:59:57 -0300

    rename

--- formula_1_etl/infra/modules/ecr/variable.tf
+++
@@ -1,7 +0,0 @@
-resoure "aws_ecr_repository" "ecr_etl_formula-1" {
-    name = "etl-formula-1",
-    image_tag_mutability = "MUTABLE"
-    image_scanning_configuration {
-        scan_on_push = true
-    }
-}
