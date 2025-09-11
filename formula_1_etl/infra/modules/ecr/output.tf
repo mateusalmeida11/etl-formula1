d6ece697c79521e02a6dd6f72db790dbdf090ad4
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-11 14:31:33 -0300

    file: correcao de nome de output

--- formula_1_etl/infra/modules/ecr/output.tf
+++ formula_1_etl/infra/modules/ecr/output.tf
@@ -1,4 +1,4 @@
-output "ecr_repoistory_url" {
+output "ecr_repository_url" {
     description = "URL de saida do repositorio do ECR"
     value = aws_ecr_repository.ecr_etl_formula_1.respository_url
 }
