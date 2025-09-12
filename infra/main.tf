b72fb530739a1261b11117aa2021de75844ba85a
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-12 10:41:24 -0300

    file: remocao de aspas

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,7 @@ module "lambda" {
     source = "./modules/lambda"
     ecr_repository_url=module.ecr.ecr_repository_url
     image_tag = var.image_tag
-    lambda_execution_role_arn" = module.iam.lambda_execution_role_arn
+    lambda_execution_role_arn = module.iam.lambda_execution_role_arn
     command_aws_lambda = var.command_aws_lambda
     lambda_layer_name = var.lambda_layer_name
     depends_on = [module.ecr]
