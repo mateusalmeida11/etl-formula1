49afa3d8b25ae3b5abeedafba72dfda6f63f4679
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-12 10:32:05 -0300

    file: criacao de variaveis opcionais

--- infra/variable.tf
+++ infra/variable.tf
@@ -6,16 +6,19 @@ variable "image_tag" {
 variable "command_aws_lambda" {
     description = "Path onde a lambda sera executada"
     type = string
+    default = ""
 
 }
 
 variable "lambda_layer_name" {
     description = "Nome da Camada da Lambda"
     type = string
+    default=""
 
 }
 
 variable "layer_name" {
     description = "Nome da Camada que Lambda Ira Executar"
     type = string
+    default=""
 }
