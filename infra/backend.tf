de8ee5d91c4daddabeed6d28b72d03b1d97942f9
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-12 10:57:06 -0300

    file: rename key do bucket

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "mateusalmeida-us-east-1-terraform-statefile-with-lock"
-    key          = "dev/statefile.tfstate"
+    key          = "formula-1-etl/statefile.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
