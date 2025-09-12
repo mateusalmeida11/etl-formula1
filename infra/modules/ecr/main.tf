5c93af27d59449889da3546ce1d63f3f63042110
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-12 10:17:21 -0300

    file: remocao de virgula

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr_etl_formula_1" {
-    name = "etl-formula-1",
+    name = "etl-formula-1"
     image_tag_mutability = "MUTABLE"
     image_scanning_configuration {
         scan_on_push = true
