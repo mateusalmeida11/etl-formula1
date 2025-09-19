049402adfaf5a77e4f8fc1280495fb04398c400b
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-19 08:34:16 -0300

    file: add tipo de pacote como image

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -1,6 +1,7 @@
 resource "aws_lambda_function" "lambda_etl" {
     function_name = var.lambda_layer_name
     role = var.lambda_execution_role_arn
+    package_type = "Image"
     image_uri = "${var.ecr_repository_url}:${var.image_tag}"
 
     image_config {
