8aeffbf5f06bd47e750f61022e7b0e941d270148
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-19 08:42:20 -0300

    file: correcao da data do version

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "lambda_role" {
     name = var.lambda_role_name
 
     assume_role_policy = jsonencode({
-        Version="2012-10-07"
+        Version="2012-10-17"
         Statement = [
             {
                 Action = "sts:AssumeRole",
