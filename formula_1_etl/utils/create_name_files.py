83d09d003678f40e1ac134cc941892ace2590f6e
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-24 16:04:50 -0300

    refactor: adicao do rounds como path

--- formula_1_etl/utils/create_name_files.py
+++ formula_1_etl/utils/create_name_files.py
@@ -8,8 +8,10 @@ def create_root_path(layer_name, category):
     )
 
 
-def create_file_name(category, season=None):
+def create_file_name(category, season=None, rounds=None):
     path_file = category
     if season:
         path_file += f"_{season}"
+    if rounds:
+        path_file += f"_round_{rounds}"
     return path_file
