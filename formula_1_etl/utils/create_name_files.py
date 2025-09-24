65c0d8677af756a2e674ee1f374a0d1bab7db72c
Mateus Almeida <mateusalmeida881@gmail.com>
2025-09-24 15:59:26 -0300

    feat: adicao de funcionalidade para nome do arquivo

--- formula_1_etl/utils/create_name_files.py
+++ formula_1_etl/utils/create_name_files.py
@@ -6,3 +6,10 @@ def create_root_path(layer_name, category):
     return (
         f"{layer_name}/{category}/year={today.year}/month={today.month}/day={today.day}"
     )
+
+
+def create_file_name(category, season=None):
+    path_file = category
+    if season:
+        path_file += f"_{season}"
+    return path_file
